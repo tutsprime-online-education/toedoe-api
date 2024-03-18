be3b5c4f7def950e73109fa10f374a3b9247ae59
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-03-18 08:48:35 +0700

    Protect the API routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@ Route::get('/', function () {
 
 Route::prefix('auth')->group(function () {
     Route::post('/login', LoginController::class)->middleware('guest');
-    Route::post('/logout', LogoutController::class);
-    Route::post('/register', RegisterController::class);
+    Route::post('/logout', LogoutController::class)->middleware('auth');
+    Route::post('/register', RegisterController::class)->middleware('guest');
 });
