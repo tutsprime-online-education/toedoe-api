4f8265426468f234bf5fc8f422696e4fd680fecb
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-03-18 08:37:12 +0700

    Add logout endpoint

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::prefix('auth')->group(function () {
-    Route::post('/login', LoginController::class);
+    Route::post('/login', LoginController::class)->middleware('guest');
+    Route::post('/logout', LogoutController::class);
 });
