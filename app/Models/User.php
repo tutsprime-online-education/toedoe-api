0896d4a9e83e821e6d755c5b706ebd0c996dae68
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-03-20 17:02:15 +0700

    filter summaries by date ranges

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Carbon\Carbon;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -50,10 +51,19 @@ class User extends Authenticatable
         return $this->hasMany(Task::class);
     }
 
-    public function tasksSummary()
+    public function tasksSummary($period = null)
     {
+        [$start, $end] = match ($period) {
+            'today' => [Carbon::now()->startOfDay(), Carbon::now()->endOfDay()],
+            'yesterday' => [Carbon::yesterday()->startOfDay(), Carbon::yesterday()->endOfDay()],
+            'lastweek', 'last-week' => [Carbon::now()->subWeek()->startOfWeek(), Carbon::now()->subWeek()->endOfWeek()],
+            'thismonth', 'this-month' => [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()],
+            'lastmonth', 'last-month' => [Carbon::now()->startOfMonth()->subMonthsNoOverflow(), Carbon::now()->subMonthsNoOverflow()->endOfMonth()],
+            default => [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()],
+        };
+        
         return $this->tasks()
-            ->where('created_at', '>=', now()->subDays(7))
+            ->whereBetween('created_at', [$start, $end])
             ->latest()
             ->get();
     }
