52ce9c190daa9458ddd528bf42f09173e2b6548d
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-03-20 16:57:51 +0700

    Add summaries endpoint

--- routes/api/v2.php
+++ routes/api/v2.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V2\TaskController;
+use App\Http\Controllers\Api\V2\SummaryController;
 use App\Http\Controllers\Api\V2\CompleteTaskController;
 
 Route::middleware('auth:sanctum')->prefix('v2')->group(function () {
     Route::apiResource('/tasks', TaskController::class);
+    Route::get('/summaries', SummaryController::class);
     Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 });
